7dbfb041b2e7432d88eedbe97e322cd1872db0a1
Anton Okhrimenko <voodoo_133@mail.ru>
2022-02-11 11:06:29 +0300

    Done 1st homework

---
+++ CleanetCode.HelloWorld.CLI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CleanetCode.HelloWorld.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Что для меня программирование?");
+            Console.WriteLine("Интересное времяпрепровождение / Работа / Способ заработка / Та польза, которую я могу приносить окружающему миру");
+            Console.WriteLine("");
+
+            Console.WriteLine("Почему я хочу стать программистом?");
+            Console.WriteLine("А я не хочу :) Я им уже стал");
+            Console.WriteLine("");
+        }
+    }
+}
